0781b82f28ca2d7341f4f48c2464b801db36e0dd
Marevan Conti <marevanconti@gmail.com>
2025-02-06 14:51:04 +0100

    Fix mysqlConnection

--- mongodb/config.js
+++ mongodb/config.js
@@ -12,6 +12,15 @@ const mysqlConnection = mysql.createConnection({
     database: process.env.MYSQL_DB_NAME,
 });
 
+mysqlConnection.connect(err => {
+    if (err) {
+        console.error("Erreur de connexion à MySQL :", err);
+        process.exit(1); // Arrête le serveur en cas d'erreur critique
+    } else {
+        console.log("✅ Connecté à MySQL !");
+    }
+});
+
 const mongoClient = new MongoClient(process.env.MONGO_DB_URL);
 
 module.exports = { mysqlConnection, mongoClient };
