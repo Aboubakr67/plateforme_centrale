9f12d8b0f29626a3ee3901d4086a8098590e8303
Aboubakr <aboubakr.zennir@gmail.com>
2025-02-08 11:09:24 +0100

    script_v3 == script final à exécuter

--- backend/script_v3.sql
+++ backend/script_v3.sql
@@ -294,3 +294,10 @@ BEGIN
 END //
 
 DELIMITER ;
+
+
+CREATE USER 'mongodb'@'localhost' IDENTIFIED BY 'mongodb';
+GRANT SELECT ON plateforme_centrale.* TO 'mongodb'@'localhost';
+GRANT EXECUTE ON PROCEDURE plateforme_centrale.produits_sportsalut TO 'mongodb'@'localhost';
+GRANT EXECUTE ON PROCEDURE plateforme_centrale.produits_gamez TO 'mongodb'@'localhost';
+GRANT EXECUTE ON PROCEDURE plateforme_centrale.produits_medidonc TO 'mongodb'@'localhost';
